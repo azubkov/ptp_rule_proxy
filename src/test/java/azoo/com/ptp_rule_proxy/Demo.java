c7296c98b31c9b5854cd38aea709eaf0e2f4f727
azubkov <artem.zubkoff@gmail.com>
2014-07-31 00:44:17 +0300

    maven generates xml objects

---
+++ src/test/java/azoo/com/ptp_rule_proxy/Demo.java
@@ -0,0 +1,33 @@
+package azoo.com.ptp_rule_proxy;
+
+
+import com.google.common.io.Resources;
+
+import azoo.com.ptp_rule_proxy.generated.RootType;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+import java.net.URL;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+        JAXBContext jc = JAXBContext.newInstance(RootType.class);
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+
+        URL url = Resources.getResource("processor_29-07-14.xml");
+        JAXBElement<RootType> jaxbRoot = (JAXBElement<RootType>) unmarshaller.unmarshal(url);
+        RootType rootType = jaxbRoot.getValue();
+        System.err.println("object: " + rootType);
+
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//        marshaller.marshal(rootType, System.out);
+        /*see  azoo.com.ptp_rule_proxy.rules.generated.ObjectFactory  _Root_QNAME*/
+        marshaller.marshal(new JAXBElement(new QName("", "root"), RootType.class, rootType), System.out);
+    }
+
+}
