756c27f3c1d767bd352070e0c4495ed4d8e11888
azubkov <artem.zubkoff@gmail.com>
2014-10-25 23:45:44 +0300

    minor cleanup

--- src/test/java/azoo/com/ptp_rule_proxy/Demo.java
+++ src/test/java/azoo/com/ptp_rule_proxy/Demo.java
@@ -3,19 +3,35 @@ package azoo.com.ptp_rule_proxy;
 import azoo.com.ptp_rule_proxy.generated.RootType;
 import azoo.com.ptp_rule_proxy.xml.FileLoader;
 import azoo.com.ptp_rule_proxy.xml.ReplacementBuilder;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Demo {
+    private static FileLoader fileLoader;
+    private static ReplacementBuilder replacementBuilder;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        fileLoader = new FileLoader();
+        replacementBuilder = new ReplacementBuilder();
+        replacementBuilder.setFileLoader(fileLoader);
+        replacementBuilder.afterPropertiesSet();
+    }
 
     @Test
     public void test0() throws Exception {
-        FileLoader fileLoader = new FileLoader();
-        ReplacementBuilder replacementBuilder = new ReplacementBuilder();
-        replacementBuilder.afterPropertiesSet();
         String xml = fileLoader.readResource("ptpxml/demo_29-07-14.xml");
         RootType rootType = replacementBuilder.toReplacement(xml);
         System.err.println("rootType: " + rootType);
+        String string = replacementBuilder.toXml(rootType);
+        System.err.println("string: " + string);
+    }
 
+    @Test
+    public void test1() throws Exception {
+        String xml = fileLoader.readContent("ptpxml/demo_29-07-14.xml");
+        RootType rootType = replacementBuilder.toReplacement(xml);
+        System.err.println("rootType: " + rootType);
         String string = replacementBuilder.toXml(rootType);
         System.err.println("string: " + string);
     }
